826e1da050d076c061c836c8d4202156e4e791a6
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2021-05-18 20:26:12 +0530

    added

---
+++ Code_Sample/7-Alternative_export.js
@@ -0,0 +1,7 @@
+module.exports.sports = ["Football", "Cricket", "Hockey", "Boxing", "Skating"];
+
+const players = {
+    palyer_name: ["Griezman", "Owens", "Diego Maradona", "Lampard", "Casillas"]
+};
+
+module.exports.my_playerList = players;
